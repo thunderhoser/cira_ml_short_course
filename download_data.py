d86c27eab592a6ac0bf9e3df1a7a3ee414047f94
thunderhoser <lagerqui@ualberta.ca>
2020-08-11 13:03:09 -0600

    Removed dependencies on GewitterGefahr.

--- download_data.py
+++ download_data.py
@@ -6,9 +6,9 @@ https://github.com/djgagne/ams-ml-python-course/blob/master/download_data.py
 """
 
 import os
+import errno
+import tarfile
 from urllib.request import urlretrieve
-from gewittergefahr.gg_utils import unzipping
-from gewittergefahr.gg_utils import file_system_utils
 
 LOCAL_DIRECTORY_NAME = 'data'
 ONLINE_TABULAR_FILE_NAME = (
@@ -21,15 +21,42 @@ ONLINE_IMAGE_FILE_NAME = (
 )
 
 
+def _mkdir_recursive_if_necessary(directory_name=None, file_name=None):
+    """Creates directory if necessary (i.e., doesn't already exist).
+
+    This method checks for the argument `directory_name` first.  If
+    `directory_name` is None, this method checks for `file_name` and extracts
+    the directory.
+
+    :param directory_name: Path to local directory.
+    :param file_name: Path to local file.
+    """
+
+    if directory_name is None:
+        assert isinstance(file_name, str)
+        directory_name = os.path.dirname(file_name)
+    else:
+        assert isinstance(directory_name, str)
+
+    if directory_name == '':
+        return
+
+    try:
+        os.makedirs(directory_name)
+    except OSError as this_error:
+        if this_error.errno == errno.EEXIST and os.path.isdir(directory_name):
+            pass
+        else:
+            raise
+
+
 def _run():
     """Downloads storm data used to train and evaluate ML models.
 
     This is effectively the main method.
     """
 
-    file_system_utils.mkdir_recursive_if_necessary(
-        directory_name=LOCAL_DIRECTORY_NAME
-    )
+    _mkdir_recursive_if_necessary(directory_name=LOCAL_DIRECTORY_NAME)
 
     local_tabular_file_name = '{0:s}/{1:s}'.format(
         LOCAL_DIRECTORY_NAME, os.path.split(ONLINE_TABULAR_FILE_NAME)[-1]
@@ -43,16 +70,14 @@ def _run():
     print('Downloading data to: "{0:s}"...\n'.format(local_image_file_name))
     urlretrieve(ONLINE_IMAGE_FILE_NAME, local_image_file_name)
 
-    unzipping.unzip_tar(
-        tar_file_name=local_tabular_file_name,
-        target_directory_name=LOCAL_DIRECTORY_NAME
-    )
+    tar_file_handle = tarfile.open(local_tabular_file_name)
+    tar_file_handle.extractall(LOCAL_DIRECTORY_NAME)
+    tar_file_handle.close()
     print('\n')
 
-    unzipping.unzip_tar(
-        tar_file_name=local_image_file_name,
-        target_directory_name=LOCAL_DIRECTORY_NAME
-    )
+    tar_file_handle = tarfile.open(local_image_file_name)
+    tar_file_handle.extractall(LOCAL_DIRECTORY_NAME)
+    tar_file_handle.close()
 
 
 if __name__ == '__main__':
